71a2b5b97e29e569c4a7a38a9ca8efa674147ef2
choijaeho <dhqkgbs@naver.com>
2021-06-08 15:04:01 +0900

    test01

--- cos Pro 1급/test01/src/sol05.java
+++ cos Pro 1급/test01/src/sol05.java
@@ -2,14 +2,24 @@
 public class sol05 {
 
 	public int solution(int n) {
-		int[][] arr = new int[n][n];
-		int i = 0, j = 0;
-		int dir = 0; // dir : 0 -> → , dir : 1 -> ↓, dir : 2 -> ←, dir : 3 -> ↑
-		int num = 1;
-
-		while (true) {
-			arr[i][j] = num;
-
+		int answer = 0;
+		
+		int arr[][] = new int[n][n];
+	
+		int dir = 0; //0 왼쪽, 1 아래, 2 위로, 3 아래로 가 반복
+		int num = 1; //첫수
+		
+		int i = 0 ; 
+		int j = 0;
+		while(true){
+			arr[i][j] = num;		
+			
+			//숫자가 끝나면 끝낸다. 
+			if(num == n*n) break;
+			
+			//방향이 끝나면, dir이 변경될 수 있도록 구현
+			//첫번쨰 끝일경우와 한바퀴더돌고 끝인 경우도 구분해야됨. --갈려는쪽이 0이 아니면 막혔으니깐 그만해야됨.
+			// 방향을 체크 하고, 끝인지 판단  || 끝이 아니고, 다음방향이  0이 아니면 방향변경, 0이면 그냥 가면되니깐. 
 			if (dir == 0 && (j == n - 1 || (j < n - 1 && arr[i][j + 1] != 0)))
 				dir++;
 			else if (dir == 1 && (i == n - 1 || (i < n - 1 && arr[i + 1][j] != 0)))
@@ -18,29 +28,18 @@ public class sol05 {
 				dir++;
 			else if (dir == 3 && (i == 0 || (i > 0 && arr[i - 1][j] != 0)))
 				dir = 0;
-
-			if (dir == 0)
-				j++;
-			else if (dir == 1)
-				i++;
-			else if (dir == 2)
-				j--;
-			else if (dir == 3)
-				i--;
-
-			if (num == (int) Math.pow(n, 2))
-				break;
+			
+			//방향에 따라 변경 
+			if(dir == 0) j++;
+			else if(dir == 1) i++;
+			else if(dir == 2) j--;
+			else if(dir == 3) i--;
+			
 			num++;
 		}
-
-		int answer = 0;
-		i = 0;
-
-		while (i <= n - 1) {
-			answer += arr[i][i];
-			i++;
+		for(int k = 0 ; k < n ; k++){
+			answer += arr[k][k];
 		}
-
 		return answer;
 	}
 }
--- cos Pro 1급/test01/src/sol09.java
+++ cos Pro 1급/test01/src/sol09.java
@@ -1,28 +1,33 @@
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class sol09 {
-	public int solution(int[] recordA, int[] recordB){
-        //보 2 가위 0 바위 1 
-	      //보 0 가위 1 바위 2 
-				//보 2 가위 0  win 보 
-	      //012
-				//0 1(2)  -> 1 
-	   		//0 2(0)  -> 0 
-				//1 2(0)  -> 2
-
-        int cnt = 0;
-        for(int i = 0; i < recordA.length; i++){
-            if(recordA[i] == recordB[i]) //비기면
-                continue;
-            else if(recordA[i] == func(recordB[i])) //이기면
-                cnt = cnt + 3;
-            else
-                cnt =  Math.max(0, cnt - 1);; //지면
-        }
-        return cnt;
+	public int func(int record){
+        if(record == 0) return 1;  //가위이면, 1
+        else if(record == 1) return 2; //바위 이면 2
+        return 0; //보면 0 
     }
+	public int solution(int[] recordA, int[] recordB) {
+		// 보 2 가위 0 바위 1
+		// 보 0 가위 1 바위 2
+		// 보 2 가위 0 win 보
+		// 012
+		// 0 1(2) -> 1
+		// 0 2(0) -> 0
+		// 1 2(0) -> 2
+
+		int cnt = 0;
+		for (int i = 0; i < recordA.length; i++) {
+			if (recordA[i] == recordB[i]) // 비기면
+				continue;
+			else if (recordA[i] == func(recordB[i])) // 이기면
+				cnt = cnt + 3;
+			else
+				cnt = Math.max(0, cnt - 1);
+			; // 지면
+		}
+		return cnt;
+	}
 }
